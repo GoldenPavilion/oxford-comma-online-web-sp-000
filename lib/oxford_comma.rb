853083f14a753e26a6064a72a3053b5891d8654e
Robert Stine <rcorystine@gmail.com>
2020-12-17 22:28:37 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,18 +1,7 @@
-# def oxford_comma(array)
-  # if array.length == 2 
-    #array.join (" and ")
-  #elsif array.legth >= 3
-    #array[-1] = "and #{array[-1]"
-    #return array.join(", ")
-  #end
-#end
-
 def oxford_comma(array)
-  if array.length == 1
-    return "#{array[0]}"
-  elsif array.length == 2
-    return array.join(" and ")
-  elsif array.length >= 3
+   if array.length == 2 
+    array.join (" and ")
+  elsif array.legth >= 3
     array[-1] = "and #{array[-1]}"
     return array.join(", ")
   end
